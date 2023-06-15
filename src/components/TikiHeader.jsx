e1dbbc5f7178ff40b99b55545b63f76c9f17c894
pillz9408 <pillz9408@gmail.com>
2023-06-15 18:01:53 +0900

    230615

---
+++ src/components/TikiHeader.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
+
+function TikiHeader(){
+    console.group("TikiHeader");
+    console.log("[Start] TikiHeader------------------");
+    
+}
---
+++ src/routes/index.jsx
@@ -0,0 +1,2 @@
+import React, { useEffect, useState, useRef, useCallback } from 'react';
+import { Navigate, Routes, Route, useLocation } from 'react-router-dom';
