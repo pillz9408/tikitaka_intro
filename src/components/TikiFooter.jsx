a0312573495b0a2b92f36f7521170942766f5248
pillz9408 <pillz9408@gmail.com>
2023-06-21 17:22:31 +0900

    230621

--- src/components/TikiFooter.jsx
+++ src/components/TikiFooter.jsx
@@ -7,10 +7,8 @@ function TikiFooter() {
                 <div className="inner">
                     <div className="info">
                         <p className='footer_txt'>
-                            <strong>(주) 오렌지 픽셀 </strong> <br />
-                            문의 메일 : ceo@orangepixel.co.kr | 전화 번호 : 010 3871 7182<br/>
-                            주소 : 서울 강남구 테헤란로53길 57-14 오빌벤쳐타워 407호
-
+                            <strong>(주) 오렌지픽셀 </strong> <br />
+                            문의 메일 : ceo@orangepixel.co.kr | 전화 번호 : 010-3871-7182
                         </p>
                         <p className="copy">© 2023. orangepixel all rights reserved </p>
                     </div>
